cd34d546c72cc031f8638fecc3ad46eb68a7f252
lcz <1543812491@qq.com>
2023-06-25 20:10:58 +0800

    add execa function

--- cli/index.js
+++ cli/index.js
@@ -1,17 +1,25 @@
 import { mkdirSync, writeFileSync } from 'fs'
 import createTemplate from './createTemplate.js'
+import questions from './questions/index.js'
+import { createConfig } from './config.js'
+import createPackageTemplate from './createPackageTemplate.js'
+import execa from 'execa'
 
-const inputConfig = {
-  packageName: 'hei',
-  port: '8080',
-  middleware: {
-    static1: true,
-    views: true,
-    router: true,
-    body: true
-  }
-}
+const answer = await questions()
+const inputConfig = createConfig(answer)
+
+// const inputConfig = {
+//   packageName: 'hei',
+//   port: '8080',
+//   middleware: {
+//     static1: true,
+//     views: true,
+//     router: true,
+//     body: true
+//   }
+// }
 
 mkdirSync('./hei')
 writeFileSync('./hei/index.ts', createTemplate(inputConfig))
-writeFileSync('./hei/package.json', 'package')
+writeFileSync('./hei/package.json', createPackageTemplate(inputConfig))
+execa('yarn', { cwd: './hei', stdio: [2, 2, 2] })
---
+++ hei/index.ts
@@ -0,0 +1,27 @@
+const Koa = require("koa");
+
+
+
+
+
+
+
+
+
+
+const app = new Koa();
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(9000, () => {
+  console.log("open server localhost:9000");
+});
